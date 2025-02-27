2dbcc2ffcead66b297afe6458c6d7b7bc7e681e6
nibirkit <nikita@nkorovin.ru>
2025-02-27 19:59:51 +0300

    Initial Commit

---
+++ Check My Meds/Check_My_MedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check_My_MedsApp.swift
+//  Check My Meds
+//
+//  Created by Nikita Korovin on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Check_My_MedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Check My MedsTests/Check_My_MedsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Check_My_MedsTests.swift
+//  Check My MedsTests
+//
+//  Created by Nikita Korovin on 27.02.2025.
+//
+
+import Testing
+@testable import Check_My_Meds
+
+struct Check_My_MedsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
